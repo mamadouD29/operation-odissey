dd7e17265c0b10d5a5216d8a4e3a3822d6fad88e
mamadouD29 <mamadoudoumbia2991@gmail.com>
2023-09-28 22:35:53 -0500

    first commit

---
+++ styles/globalStyles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	hCtr: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 5,
+	},
+	vCtr: {
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 5,
+	},
+    result: {
+        fontSize: 36,
+    },
+    inp: {
+        fontSize: 20,
+    }
+    
+});
